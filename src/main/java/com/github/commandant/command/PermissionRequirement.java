7738766b0f06227a7b503ddfe4b6634b4f853b99
Shotbyte <mdeancline@outlook.com>
2022-05-28 15:27:58 -0500

    Add IdentifiableCommand compatibility

--- src/main/java/com/github/commandant/command/PermissionRequirement.java
+++ src/main/java/com/github/commandant/command/PermissionRequirement.java
@@ -1,1 +1,61 @@
 package com.github.commandant.command;
+
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * @param <T>
+ */
+public class PermissionRequirement<T extends CommandSender> implements IdentifiableCommand<T> {
+    private final Command<T> innerCommand;
+    private final String requiredPermission;
+    private final String noPermissionMessage;
+    private final Supplier<String> description;
+    private final Supplier<List<String>> aliases;
+
+    public PermissionRequirement(final Command<T> innerCommand, final String requiredPermission, final String noPermissionMessage) {
+        this(innerCommand, requiredPermission, noPermissionMessage, null, null);
+    }
+
+    public PermissionRequirement(final IdentifiableCommand<T> innerCommand, final String requiredPermission, final String noPermissionMessage) {
+        this(innerCommand, requiredPermission, noPermissionMessage, innerCommand::getDescription, innerCommand::getAliases);
+    }
+
+    private PermissionRequirement(final Command<T> innerCommand, final String requiredPermission, final String noPermissionMessage, final Supplier<String> description, final Supplier<List<String>> aliases) {
+        this.innerCommand = innerCommand;
+        this.requiredPermission = requiredPermission;
+        this.noPermissionMessage = noPermissionMessage;
+        this.description = description;
+        this.aliases = aliases;
+    }
+
+    @Override
+    public final void execute(final T sender, final String[] args) {
+        if (sender.hasPermission(requiredPermission))
+            innerCommand.execute(sender, args);
+        else if (noPermissionMessage != null && !noPermissionMessage.isEmpty())
+            sender.sendMessage(noPermissionMessage.replace("%permission", requiredPermission));
+    }
+
+    @Override
+    public final List<String> tabComplete(final T sender, final String[] args) {
+        return innerCommand.tabComplete(sender, args);
+    }
+
+    @Override
+    public final String getLabel() {
+        return innerCommand.getLabel();
+    }
+
+    @Override
+    public final String getDescription() {
+        return description.get();
+    }
+
+    @Override
+    public final List<String> getAliases() {
+        return aliases.get();
+    }
+}
