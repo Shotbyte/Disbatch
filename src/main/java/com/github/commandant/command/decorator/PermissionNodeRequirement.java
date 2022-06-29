89b1875cbeaad6838c9664249861c4aae9702ba7
Shotbyte <mdeancline@outlook.com>
2022-06-28 23:03:27 -0500

    Refactor with clearer intent

--- src/main/java/com/github/commandant/command/decorator/PermissionNodeRequirement.java
+++ src/main/java/com/github/commandant/command/decorator/PermissionNodeRequirement.java
@@ -11,27 +11,27 @@ import java.util.Objects;
 /**
  * @param <T>
  */
-public final class PermissionRequirement<T extends CommandSender> extends CommandProxy<T> {
-    private final String requiredPermission;
+public final class PermissionNodeRequirement<T extends CommandSender> extends CommandProxy<T> {
+    private final String requiredPermissionNode;
     private final String noPermissionMessage;
 
-    public PermissionRequirement(final Command<T> innerCommand, final String requiredPermission, final String noPermissionMessage) {
+    public PermissionNodeRequirement(final Command<T> innerCommand, final String requiredPermissionNode, final String noPermissionMessage) {
         super(innerCommand);
-        this.requiredPermission = Objects.requireNonNull(requiredPermission);
+        this.requiredPermissionNode = Objects.requireNonNull(requiredPermissionNode);
         this.noPermissionMessage = noPermissionMessage;
     }
 
     @Override
     public void execute(final T sender, final String commandLabel, final String[] args) {
-        if (sender.hasPermission(requiredPermission))
+        if (sender.hasPermission(requiredPermissionNode))
             super.execute(sender, commandLabel, args);
         else if (noPermissionMessage != null && !noPermissionMessage.isEmpty())
-            sender.sendMessage(noPermissionMessage.replace("%permission", requiredPermission));
+            sender.sendMessage(noPermissionMessage.replace("%permission", requiredPermissionNode));
     }
 
     @Override
     public List<String> tabComplete(final T sender, final String[] args) {
-        return sender.hasPermission(requiredPermission)
+        return sender.hasPermission(requiredPermissionNode)
                 ? super.tabComplete(sender, args)
                 : Collections.emptyList();
     }
